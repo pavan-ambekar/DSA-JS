7d59149ce093df82b9b1cf9f5d69859737f8a158
Pavan A <pavan479ec@gmail.com>
2023-12-20 01:05:22 +0530

    Day-07

--- Data Structures/04-Linked List.js
+++ Data Structures/04-Linked List.js
@@ -61,6 +61,79 @@ class LinkedList {
         }
     }
 
+    removeFrom(index) {
+        if (index < 0 || index >= this.size) {
+            return null
+        }
+        let removedNode
+        if (index === 0) {
+            removedNode = this.head
+            this.head = this.head.next
+        } else {
+            let prev = this.head
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next
+            }
+            removedNode = prev.next
+            prev.next = removedNode.next
+
+        }
+
+        this.size--
+        return removedNode.value
+    }
+
+    removeValue(value) {
+        if (this.isEmpty()) {
+            return null
+        } else if (this.head.value === value) {
+            this.head = this.head.next
+            this.size--
+            return value
+        } else {
+            let prev = this.head
+            while (prev.next) {
+                if (prev.next.value === value) {
+                    const removedNode = prev.next
+                    prev.next = removedNode.next
+                    this.size--
+                    return value
+                }
+                prev = prev.next
+            }
+            return null
+        }
+    }
+
+    search(value) {
+        if (this.isEmpty()) {
+            return -1
+        } else {
+            let current = this.head
+            let i = 0
+            while (current) {
+                if (current.value === value) {
+                    return i
+                }
+                current = current.next
+                i++
+            }
+            return -1
+        }
+    }
+
+    reverse() {
+        let prev = null
+        let current = this.head
+        while (current) {
+            let next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        }
+        this.head = prev
+    }
+
     print() {
         if (this.isEmpty()) {
             console.log('List is empty.')
@@ -96,4 +169,21 @@ list.print()
 list.insert(0, 2)
 list.insert(7, 25)
 
+list.print()
+
+console.log(list.removeFrom(10))
+list.print()
+console.log(list.removeFrom(6))
+list.print()
+
+console.log(list.removeValue(2))
+list.print()
+console.log(list.removeValue(33))
+list.print()
+
+console.log(list.search(33))
+console.log(list.search(30))
+console.log(list.search(25))
+
+list.reverse()
 list.print()
